d25ccd337b64e57c2da741ff243cd7798ef62c6c
Lukas Karberg <deaderlp@googlemail.com>
2024-02-22 10:49:55 +0100

    Add axios dependency and fetch employees data

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import { IEmployee } from "../interfaces";
+import axios from "axios";
+import * as config from "../../config";
+
 export const PageWelcome = () => {
+	const [employees, setEmployees] = useState<IEmployee[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(
+				`${config.backendUrl()}/employees`
+			);
+			const _employees = response.data as IEmployee[];
+			setEmployees(_employees);
+		})();
+	}, []);
+
 	return (
-		<p>This is the welcome page.</p>
-	)
-}
+		<>
+			<h2 className="mb-2 text-xl">
+				There are {employees.length} employees
+			</h2>
+			<ul>
+				{employees.map((employee) => (
+					<li key={employee.employeeID}>
+						<span className="font-bold">
+							{employee.firstName} {employee.lastName} -{" "}
+						</span>
+						{employee.title}
+					</li>
+				))}
+			</ul>
+		</>
+	);
+};
